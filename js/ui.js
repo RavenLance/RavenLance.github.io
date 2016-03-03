258f70551dfcfbdcc690ed9ded54c1b02c6e9f96
BloodRaven <bloodraven.linux@gmail.com>
2016-03-04 02:06:50 +1100

    Testing

--- js/ui.js
+++ js/ui.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
 function openNotes() {
 	var $htmlDoc = jQuery("#moreContent");
-	$htmlDoc.load("./notes/notes.html");
+	$htmlDoc.load("./notes/ContentStructure.html");
 }
 
 // A future function to be implemented
