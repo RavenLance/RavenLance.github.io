c9f31466ff35fb9b8c1e79944fb668873e985dfb
RavenLance <bloodraven.linux@gmail.com>
2016-03-04 10:48:00 +1100

    derp gunna derp again

--- js/ui.js
+++ js/ui.js
@@ -23,5 +23,5 @@ function fillNotes() {
 	var $htmlDoc = jQuery("#moreContent2");
 	$htmlDoc.load("./notes/HighSpeedNetworks/ContentStructure.html");
 
-	$('#moreContent2').append(#dragScript);
+	$('#moreContent2').append($dragScript);
 }
