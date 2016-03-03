15e6fef4252e8cdedf86bd6f4ec5c9c0f7d4668f
BloodRaven <bloodraven.linux@gmail.com>
2016-03-03 22:57:48 +1100

    preping test

--- js/ui.js
+++ js/ui.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 function openNotes() {
 	var $htmlDoc = jQuery("#moreContent");
-	$htmlDoc.load("./notes/notes.html");
+	$htmlDoc.load("./notes/notestest.html");
 
 	dragCreation();
 }
